039d168ff5baa1831d7016312d886a47e8da101f
Apoorv Varshney <85228469+apoorv-5369@users.noreply.github.com>
2022-07-01 15:25:40 +0530

    Update test-utils.tsx

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,13 +1,6 @@
 import * as React from "react"
-import * as ReactDOM from "react-dom"
-import { 
-  render,
-        RenderOptions 
-} from "@testing-library/react"
-import {
-  ChakraProvider,
-        theme 
-       } from "@chakra-ui/react"
+import {   render,  RenderOptions } from "@testing-library/react"
+import { ChakraProvider, theme } from "@chakra-ui/react"
 
 const AllProviders = ({ children }: { children?: React.ReactNode }) => (
   <ChakraProvider theme={theme}>{children}</ChakraProvider>
